e406e454000bf42f76fbb13d7107f0d0fb1ae63f
banteg <4562643+banteg@users.noreply.github.com>
2023-08-16 05:49:46 +0400

    fix: cryo cli build

--- crates/freeze/src/types/dataframes/sort.rs
+++ crates/freeze/src/types/dataframes/sort.rs
@@ -10,7 +10,7 @@ impl SortableDataFrame for Result<DataFrame, CollectError> {
     fn sort_by_schema(self, schema: &Table) -> Self {
         match (self, &schema.sort_columns) {
             (Ok(df), Some(sort_columns)) => {
-                df.sort(sort_columns, false).map_err(CollectError::PolarsError)
+                df.sort(sort_columns, false, false).map_err(CollectError::PolarsError)
             }
             (df, _) => df,
         }
