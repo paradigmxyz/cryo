ab70444a40b8d8e2a9f501f48055b5c44ac22936
Kamil Skalski <kamil.skalski@gmail.com>
2023-08-21 12:38:08 +0200

    Refactor, add tests and update docs.

--- crates/cli/src/args.rs
+++ crates/cli/src/args.rs
@@ -40,7 +40,9 @@ pub struct Args {
     pub reorg_buffer: u64,
 
     /// Columns to include alongside the default output,
-    /// use `all` to include all available columns
+    /// use `all` to include all available columns.
+    /// Unknown columns are ignored per dataset, so a superset of columns can be specified for
+    /// multiset download.
     #[arg(short, long, value_name="COLS", num_args(0..), verbatim_doc_comment, help_heading="Content Options")]
     pub include_columns: Option<Vec<String>>,
 
