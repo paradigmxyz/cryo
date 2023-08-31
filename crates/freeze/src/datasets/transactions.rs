eedcbeb60aeaaddfbeb807121a4a7b2d401c7b74
Kamil Skalski <kamil.skalski@gmail.com>
2023-08-31 19:02:10 +0200

    refactor: cleanup collecting and managing column data (#54)
    * Create LogsColumns and refactor transaction columns to transaction's file.
    
    * Move counting n_rows to column objects.
    
    * Formatting.

--- crates/freeze/src/datasets/transactions.rs
+++ crates/freeze/src/datasets/transactions.rs
@@ -5,9 +5,14 @@ use polars::prelude::*;
 use tokio::{sync::mpsc, task};
 
 use super::{blocks, blocks::ProcessTransactions, blocks_and_transactions};
-use crate::types::{
-    BlockChunk, CollectError, ColumnType, Dataset, Datatype, RowFilter, Source, Table,
-    TransactionChunk, Transactions,
+use crate::{
+    dataframes::SortableDataFrame,
+    types::{
+        conversions::{ToVecHex, ToVecU8},
+        BlockChunk, CollectError, ColumnType, Dataset, Datatype, RowFilter, Source, Table,
+        TransactionChunk, Transactions, U256Type,
+    },
+    with_series, with_series_binary, with_series_u256,
 };
 
 #[async_trait::async_trait]
@@ -167,21 +172,126 @@ async fn fetch_transactions(
     }
 }
 
+#[derive(Default)]
+pub(crate) struct TransactionColumns {
+    n_rows: usize,
+    block_number: Vec<Option<u64>>,
+    transaction_index: Vec<Option<u64>>,
+    transaction_hash: Vec<Vec<u8>>,
+    nonce: Vec<u64>,
+    from_address: Vec<Vec<u8>>,
+    to_address: Vec<Option<Vec<u8>>>,
+    value: Vec<U256>,
+    input: Vec<Vec<u8>>,
+    gas_limit: Vec<u32>,
+    gas_used: Vec<u32>,
+    gas_price: Vec<Option<u64>>,
+    transaction_type: Vec<Option<u32>>,
+    max_priority_fee_per_gas: Vec<Option<u64>>,
+    max_fee_per_gas: Vec<Option<u64>>,
+}
+
+impl TransactionColumns {
+    pub(crate) fn create_df(
+        self,
+        schema: &Table,
+        chain_id: u64,
+    ) -> Result<DataFrame, CollectError> {
+        let mut cols = Vec::with_capacity(schema.columns().len());
+        with_series!(cols, "block_number", self.block_number, schema);
+        with_series!(cols, "transaction_index", self.transaction_index, schema);
+        with_series_binary!(cols, "transaction_hash", self.transaction_hash, schema);
+        with_series!(cols, "nonce", self.nonce, schema);
+        with_series_binary!(cols, "from_address", self.from_address, schema);
+        with_series_binary!(cols, "to_address", self.to_address, schema);
+        with_series_u256!(cols, "value", self.value, schema);
+        with_series_binary!(cols, "input", self.input, schema);
+        with_series!(cols, "gas_limit", self.gas_limit, schema);
+        with_series!(cols, "gas_used", self.gas_used, schema);
+        with_series!(cols, "gas_price", self.gas_price, schema);
+        with_series!(cols, "transaction_type", self.transaction_type, schema);
+        with_series!(cols, "max_priority_fee_per_gas", self.max_priority_fee_per_gas, schema);
+        with_series!(cols, "max_fee_per_gas", self.max_fee_per_gas, schema);
+        with_series!(cols, "chain_id", vec![chain_id; self.n_rows], schema);
+
+        DataFrame::new(cols).map_err(CollectError::PolarsError).sort_by_schema(schema)
+    }
+
+    pub(crate) fn process_transaction(
+        &mut self,
+        tx: &Transaction,
+        schema: &Table,
+        gas_used: Option<u32>,
+    ) {
+        self.n_rows += 1;
+        if schema.has_column("block_number") {
+            match tx.block_number {
+                Some(block_number) => self.block_number.push(Some(block_number.as_u64())),
+                None => self.block_number.push(None),
+            }
+        }
+        if schema.has_column("transaction_index") {
+            match tx.transaction_index {
+                Some(transaction_index) => {
+                    self.transaction_index.push(Some(transaction_index.as_u64()))
+                }
+                None => self.transaction_index.push(None),
+            }
+        }
+        if schema.has_column("transaction_hash") {
+            self.transaction_hash.push(tx.hash.as_bytes().to_vec());
+        }
+        if schema.has_column("from_address") {
+            self.from_address.push(tx.from.as_bytes().to_vec());
+        }
+        if schema.has_column("to_address") {
+            match tx.to {
+                Some(to_address) => self.to_address.push(Some(to_address.as_bytes().to_vec())),
+                None => self.to_address.push(None),
+            }
+        }
+        if schema.has_column("nonce") {
+            self.nonce.push(tx.nonce.as_u64());
+        }
+        if schema.has_column("value") {
+            self.value.push(tx.value);
+        }
+        if schema.has_column("input") {
+            self.input.push(tx.input.to_vec());
+        }
+        if schema.has_column("gas_limit") {
+            self.gas_limit.push(tx.gas.as_u32());
+        }
+        if schema.has_column("gas_used") {
+            self.gas_used.push(gas_used.unwrap())
+        }
+        if schema.has_column("gas_price") {
+            self.gas_price.push(tx.gas_price.map(|gas_price| gas_price.as_u64()));
+        }
+        if schema.has_column("transaction_type") {
+            self.transaction_type.push(tx.transaction_type.map(|value| value.as_u32()));
+        }
+        if schema.has_column("max_priority_fee_per_gas") {
+            self.max_priority_fee_per_gas
+                .push(tx.max_priority_fee_per_gas.map(|value| value.as_u64()));
+        }
+        if schema.has_column("max_fee_per_gas") {
+            self.max_fee_per_gas.push(tx.max_fee_per_gas.map(|value| value.as_u64()));
+        }
+    }
+}
+
 async fn transactions_to_df(
     mut transactions: mpsc::Receiver<Result<(Transaction, Option<u32>), CollectError>>,
     schema: &Table,
     chain_id: u64,
 ) -> Result<DataFrame, CollectError> {
-    let mut columns = blocks::TransactionColumns::new(100);
-    let mut n_txs = 0;
+    let mut columns = TransactionColumns::default();
     while let Some(message) = transactions.recv().await {
         match message {
-            Ok((transaction, gas_used)) => {
-                n_txs += 1;
-                transaction.process(schema, &mut columns, gas_used)
-            }
+            Ok((transaction, gas_used)) => transaction.process(schema, &mut columns, gas_used),
             Err(e) => return Err(e),
         }
     }
-    columns.create_df(schema, chain_id, n_txs)
+    columns.create_df(schema, chain_id)
 }
