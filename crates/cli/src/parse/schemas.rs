af30b06402dbe14240b06c4687df0b63994ab4ee
sslivkoff <stormslivkoff@gmail.com>
2023-10-05 09:33:32 -0700

    split parse_schemas into multiple functions

--- crates/cli/src/parse/schemas.rs
+++ crates/cli/src/parse/schemas.rs
@@ -25,41 +25,17 @@ fn parse_datatypes(raw_inputs: &Vec<String>) -> Result<Vec<Datatype>, ParseError
 }
 
 pub(crate) fn parse_schemas(args: &Args) -> Result<HashMap<Datatype, Table>, ParseError> {
+    // parse inputs
     let datatypes = parse_datatypes(&args.datatype)?;
+    let sort = parse_sort_columns(&args.sort, &datatypes)?;
+    let u256_types = parse_u256_types(args)?;
     let output_format = file_output::parse_output_format(args)?;
-
-    let u256_types = if let Some(raw_u256_types) = &args.u256_types {
-        let mut u256_types: HashSet<U256Type> = HashSet::new();
-        for raw in raw_u256_types.iter() {
-            let u256_type = match raw.to_lowercase() {
-                raw if raw == "binary" => U256Type::Binary,
-                raw if raw == "string" => U256Type::String,
-                raw if raw == "str" => U256Type::String,
-                raw if raw == "f32" => U256Type::F32,
-                raw if raw == "float32" => U256Type::F32,
-                raw if raw == "f64" => U256Type::F64,
-                raw if raw == "float64" => U256Type::F64,
-                raw if raw == "float" => U256Type::F64,
-                raw if raw == "u32" => U256Type::U32,
-                raw if raw == "uint32" => U256Type::U32,
-                raw if raw == "u64" => U256Type::U64,
-                raw if raw == "uint64" => U256Type::U64,
-                raw if raw == "decimal128" => U256Type::Decimal128,
-                raw if raw == "d128" => U256Type::Decimal128,
-                _ => return Err(ParseError::ParseError("bad u256 type".to_string())),
-            };
-            u256_types.insert(u256_type);
-        }
-        u256_types
-    } else {
-        HashSet::from_iter(vec![U256Type::Binary, U256Type::String, U256Type::F64])
-    };
     let binary_column_format = match args.hex | (output_format != FileFormat::Parquet) {
         true => ColumnEncoding::Hex,
         false => ColumnEncoding::Binary,
     };
 
-    let sort = parse_sort_columns(&args.sort, &datatypes)?;
+    // create schemas
     let schemas: Result<HashMap<Datatype, Table>, ParseError> = datatypes
         .iter()
         .map(|datatype| {
@@ -85,55 +61,100 @@ pub(crate) fn parse_schemas(args: &Args) -> Result<HashMap<Datatype, Table>, Par
 
     // make sure all included columns ended up in at least one schema
     if let (Ok(schemas), Some(include_columns)) = (&schemas, &args.include_columns) {
-        let mut unknown_columns = Vec::new();
-        for column in include_columns.iter() {
-            let mut in_a_schema = false;
-
-            for schema in schemas.values() {
-                if schema.has_column(column) {
-                    in_a_schema = true;
-                    break
-                }
-            }
-
-            if !in_a_schema && column != "all" {
-                unknown_columns.push(column);
-            }
-        }
-        if !unknown_columns.is_empty() {
-            return Err(ParseError::ParseError(format!(
-                "datatypes do not support these columns: {:?}",
-                unknown_columns
-            )))
-        }
+        ensure_included_columns(include_columns, schemas)?
     };
 
     // make sure all excluded columns are excluded from at least one schema
     if let (Ok(schemas), Some(exclude_columns)) = (&schemas, &args.exclude_columns) {
-        let mut unknown_columns = Vec::new();
-        for column in exclude_columns.iter() {
-            let mut in_a_schema = false;
-
-            for datatype in schemas.keys() {
-                if datatype.column_types().contains_key(&column.as_str()) {
-                    in_a_schema = true;
-                    break
-                }
-            }
-
-            if !in_a_schema {
-                unknown_columns.push(column);
-            }
-        }
-        if !unknown_columns.is_empty() {
-            return Err(ParseError::ParseError(format!(
-                "datatypes do not support these columns: {:?}",
-                unknown_columns
-            )))
-        }
+        ensure_excluded_columns(exclude_columns, schemas)?
     };
 
     schemas
+}
+
+fn parse_u256_types(args: &Args) -> Result<HashSet<U256Type>, ParseError> {
+    if let Some(raw_u256_types) = args.u256_types.clone() {
+        let mut u256_types: HashSet<U256Type> = HashSet::new();
+        for raw in raw_u256_types.iter() {
+            let u256_type = match raw.to_lowercase() {
+                raw if raw == "binary" => U256Type::Binary,
+                raw if raw == "string" => U256Type::String,
+                raw if raw == "str" => U256Type::String,
+                raw if raw == "f32" => U256Type::F32,
+                raw if raw == "float32" => U256Type::F32,
+                raw if raw == "f64" => U256Type::F64,
+                raw if raw == "float64" => U256Type::F64,
+                raw if raw == "float" => U256Type::F64,
+                raw if raw == "u32" => U256Type::U32,
+                raw if raw == "uint32" => U256Type::U32,
+                raw if raw == "u64" => U256Type::U64,
+                raw if raw == "uint64" => U256Type::U64,
+                raw if raw == "decimal128" => U256Type::Decimal128,
+                raw if raw == "d128" => U256Type::Decimal128,
+                _ => return Err(ParseError::ParseError("bad u256 type".to_string())),
+            };
+            u256_types.insert(u256_type);
+        }
+        Ok(u256_types)
+    } else {
+        Ok(HashSet::from_iter(vec![U256Type::Binary, U256Type::String, U256Type::F64]))
+    }
+}
+
+fn ensure_included_columns(
+    include_columns: &[String],
+    schemas: &cryo_freeze::Schemas,
+) -> Result<(), ParseError> {
+    let mut unknown_columns = Vec::new();
+    for column in include_columns.iter() {
+        let mut in_a_schema = false;
+
+        for schema in schemas.values() {
+            if schema.has_column(column) {
+                in_a_schema = true;
+                break;
+            }
+        }
+
+        if !in_a_schema && column != "all" {
+            unknown_columns.push(column);
+        }
+    }
+    if !unknown_columns.is_empty() {
+        return Err(ParseError::ParseError(format!(
+            "datatypes do not support these columns: {:?}",
+            unknown_columns
+        )));
+    }
+    Ok(())
+}
+
+fn ensure_excluded_columns(
+    exclude_columns: &[String],
+    schemas: &cryo_freeze::Schemas,
+) -> Result<(), ParseError> {
+    let mut unknown_columns = Vec::new();
+    for column in exclude_columns.iter() {
+        let mut in_a_schema = false;
+
+        for datatype in schemas.keys() {
+            if datatype.column_types().contains_key(&column.as_str()) {
+                in_a_schema = true;
+                break;
+            }
+        }
+
+        if !in_a_schema {
+            unknown_columns.push(column);
+        }
+    }
+    if !unknown_columns.is_empty() {
+        return Err(ParseError::ParseError(format!(
+            "datatypes do not support these columns: {:?}",
+            unknown_columns
+        )));
+    }
+    Ok(())
 }
 
 fn parse_sort_columns(
