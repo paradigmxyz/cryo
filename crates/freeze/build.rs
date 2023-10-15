bd817c801255609d0d5bac1937853e6b3af4133c
sslivkoff <stormslivkoff@gmail.com>
2023-10-15 12:06:07 -0700

    cargo fmt

--- crates/freeze/build.rs
+++ crates/freeze/build.rs
@@ -1,16 +1,14 @@
 use std::process::Command;
 
 fn main() {
-    let git_description = get_git_description()
-        .unwrap_or_else(|_| env!("CARGO_PKG_VERSION").to_string());
+    let git_description =
+        get_git_description().unwrap_or_else(|_| env!("CARGO_PKG_VERSION").to_string());
 
     println!("cargo:rustc-env=GIT_DESCRIPTION={}", git_description);
 }
 
 fn get_git_description() -> Result<String, std::io::Error> {
-    let output = Command::new("git")
-        .args(["describe", "--tags", "--always"])
-        .output()?;
+    let output = Command::new("git").args(["describe", "--tags", "--always"]).output()?;
 
     if output.status.success() {
         let description = String::from_utf8(output.stdout)
@@ -20,9 +18,6 @@ fn get_git_description() -> Result<String, std::io::Error> {
 
         Ok(description)
     } else {
-        Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "Git command failed",
-        ))
+        Err(std::io::Error::new(std::io::ErrorKind::Other, "Git command failed"))
     }
 }
