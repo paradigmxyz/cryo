8c1516787679a8c722c5cbfafd174f0de4476eab
fahimahmedx <f92ahmed@uwaterloo.ca>
2023-11-14 23:24:13 -0500

    add get_latest_block_number() function

--- crates/cli/src/parse/timestamp.rs
+++ crates/cli/src/parse/timestamp.rs
@@ -1,6 +1,20 @@
 use ethers::prelude::*;
+use polars::prelude::*;
 use cryo_freeze::{BlockChunk, Fetcher, ParseError, ChunkData};
+
+use crate::Args;
 
 pub async fn timestamp_to_block<P: JsonRpcClient>(timestamp: u32, fetcher: &Fetcher<P>) -> Result<u64, ParseError> {
     todo!()
+}
+
+async fn get_latest_block_number<P: JsonRpcClient>(
+    args: &Args,
+    fetcher: Arc<Fetcher<P>>
+) -> Result<u64, ParseError> {
+    let (_, block_numbers) = crate::parse::blocks::parse_blocks(args, fetcher.clone()).await?;
+    let block_numbers = block_numbers.unwrap();
+    let latest_block_numbers = block_numbers[block_numbers.len() - 1].values();
+    
+    return Ok(latest_block_numbers[latest_block_numbers.len() - 1]);
 }
