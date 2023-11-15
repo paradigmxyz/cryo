ab2ed6bbaf4cb9bb9a480b8349efdfc80c2c0c9d
fahimahmedx <f92ahmed@uwaterloo.ca>
2023-11-15 00:56:09 -0500

    Add timestamp_to_block_number() function

--- crates/cli/src/parse/timestamp.rs
+++ crates/cli/src/parse/timestamp.rs
@@ -1,11 +1,38 @@
 use ethers::prelude::*;
 use polars::prelude::*;
-use cryo_freeze::{BlockChunk, Fetcher, ParseError, ChunkData};
+use cryo_freeze::{BlockChunk, Fetcher, ParseError, ChunkData, CollectError};
 
 use crate::Args;
 
-pub async fn timestamp_to_block<P: JsonRpcClient>(timestamp: u32, fetcher: &Fetcher<P>) -> Result<u64, ParseError> {
-    todo!()
+pub async fn timestamp_to_block_number<P: JsonRpcClient>(timestamp: u64, args: &Args, fetcher: Arc<Fetcher<P>>) -> Result<u64, CollectError> {
+    let mut block_numbers: Vec<u64> = Vec::new();
+    let latest_block_number = get_latest_block_number(args, fetcher.clone()).await?;
+
+    for block_number in 0..latest_block_number {
+        block_numbers.push(block_number);
+    }
+
+    // perform binary search to determine the block number for a given timestamp.
+    // If the exact timestamp is not found, we return the closest block number.
+    let mut l = 0;
+    let mut r = block_numbers.len() as u64 - 1; 
+    let mut mid = l + (r - 1) / 2;
+    let mut block = fetcher.get_block(mid).await?.unwrap();
+
+    while l <= r {
+        mid = l + (r - 1) / 2;
+        block = fetcher.get_block(mid).await?.unwrap();
+
+        if block.timestamp == timestamp.into() {
+            return Ok(mid);
+        } else if block.timestamp < timestamp.into() {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+
+    return Ok(mid);
 }
 
 async fn get_latest_block_number<P: JsonRpcClient>(
@@ -15,6 +42,6 @@ async fn get_latest_block_number<P: JsonRpcClient>(
     let (_, block_numbers) = crate::parse::blocks::parse_blocks(args, fetcher.clone()).await?;
     let block_numbers = block_numbers.unwrap();
     let latest_block_numbers = block_numbers[block_numbers.len() - 1].values();
-    
+
     return Ok(latest_block_numbers[latest_block_numbers.len() - 1]);
 }
