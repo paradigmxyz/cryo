273d386a62429fcf6cfdc37d48240838fcdaadfc
fahimahmedx <f92ahmed@uwaterloo.ca>
2023-11-15 20:37:49 -0500

    use reference, fix get_latest_block_number()

--- crates/cli/src/parse/timestamp.rs
+++ crates/cli/src/parse/timestamp.rs
@@ -2,11 +2,12 @@ use ethers::prelude::*;
 use polars::prelude::*;
 use cryo_freeze::{BlockChunk, Fetcher, ParseError, ChunkData, CollectError};
 
-use crate::Args;
-
-pub async fn timestamp_to_block_number<P: JsonRpcClient>(timestamp: u64, args: &Args, fetcher: Arc<Fetcher<P>>) -> Result<u64, CollectError> {
+async fn timestamp_to_block_number<P: JsonRpcClient>(
+    timestamp: u64,
+    fetcher: &Fetcher<P>
+) -> Result<u64, CollectError> {
     let mut block_numbers: Vec<u64> = Vec::new();
-    let latest_block_number = get_latest_block_number(args, fetcher.clone()).await?;
+    let latest_block_number = get_latest_block_number(fetcher).await?;
 
     for block_number in 0..latest_block_number {
         block_numbers.push(block_number);
@@ -15,7 +16,7 @@ pub async fn timestamp_to_block_number<P: JsonRpcClient>(timestamp: u64, args: &
     // perform binary search to determine the block number for a given timestamp.
     // If the exact timestamp is not found, we return the closest block number.
     let mut l = 0;
-    let mut r = block_numbers.len() as u64 - 1; 
+    let mut r = block_numbers.len() as u64 - 1;
     let mut mid = l + (r - 1) / 2;
     let mut block = fetcher.get_block(mid).await?.unwrap();
 
@@ -36,12 +37,11 @@ pub async fn timestamp_to_block_number<P: JsonRpcClient>(timestamp: u64, args: &
 }
 
 async fn get_latest_block_number<P: JsonRpcClient>(
-    args: &Args,
-    fetcher: Arc<Fetcher<P>>
+    fetcher: &Fetcher<P>,
 ) -> Result<u64, ParseError> {
-    let (_, block_numbers) = crate::parse::blocks::parse_blocks(args, fetcher.clone()).await?;
-    let block_numbers = block_numbers.unwrap();
-    let latest_block_numbers = block_numbers[block_numbers.len() - 1].values();
-
-    return Ok(latest_block_numbers[latest_block_numbers.len() - 1]);
+    return fetcher
+        .get_block_number()
+        .await
+        .map(|n| n.as_u64())
+        .map_err(|_e| ParseError::ParseError("Error retrieving latest block number".to_string()));
 }
