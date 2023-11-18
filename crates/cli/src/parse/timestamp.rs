4e5551e7b034bf1629a19944556470e5a8bfd698
fahimahmedx <f92ahmed@uwaterloo.ca>
2023-11-18 01:06:40 -0500

    remove unused array in timestamp_to_block_numbers()

--- crates/cli/src/parse/timestamp.rs
+++ crates/cli/src/parse/timestamp.rs
@@ -270,17 +270,12 @@ async fn timestamp_to_block_number<P: JsonRpcClient>(
     timestamp: u64,
     fetcher: &Fetcher<P>,
 ) -> Result<u64, ParseError> {
-    let mut block_numbers: Vec<u64> = Vec::new();
     let latest_block_number = get_latest_block_number(fetcher).await?;
-
-    for block_number in 0..latest_block_number {
-        block_numbers.push(block_number);
-    }
 
     // perform binary search to determine the block number for a given timestamp.
     // If the exact timestamp is not found, we return the closest block number.
     let mut l = 0;
-    let mut r = block_numbers.len() as u64 - 1;
+    let mut r = latest_block_number;
     let mut mid = l + (r - 1) / 2;
     let mut block = fetcher
         .get_block(mid)
