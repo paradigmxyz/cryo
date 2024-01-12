123091a4389089cd0aef2ec331e520d447f3a007
sslivkoff <stormslivkoff@gmail.com>
2024-01-12 09:23:26 -0800

    update erc20 balances example

--- examples/erc20_balances.sh
+++ examples/erc20_balances.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
 
-# get ERC20 balance of a wallet over some block range
+# get ERC20 balances of addresses over some block range
 
-# ERC20 = USDC
-export CONTRACT=0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48
+# ERC20 = USDC, USDT
+export ERC20s="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48 0xdac17f958d2ee523a2206206994597c13d831ec7"
 
-# Wallet = Maker PSM
-export WALLET=0x0a59649758aa4d66e25f08dd01271e891fe52199
-
-# selector for balanceOf(address)
-export FUNCTION=0x70a08231
+# Wallet = Maker PSM, Arbitrum Brdige
+export WALLETS="0x0a59649758aa4d66e25f08dd01271e891fe52199 0xcee284f754e854890e311e3280b767f80797180d"
 
 # block range = 10 blocks between 16,000,000 and 17,000,000
 export BLOCKS="16M:17M/10"
 
-cryo eth_calls \
-    --label erc20_balances \
-    --blocks $BLOCKS \
-    --contract $CONTRACT \
-    --function $FUNCTION \
-    --inputs $(printf '%064s' ${WALLET:2})
+# collect data
+cryo erc20_balances --blocks $BLOCKS --contract $CONTRACT --address $WALLET
 
