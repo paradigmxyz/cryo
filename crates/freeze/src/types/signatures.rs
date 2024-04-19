24fc2f772df6b0c612ff3710da2d7f77972b0a48
Yadomin <i@yadom.in>
2024-04-20 01:42:04 +0800

    Add erc20_approvals dataset (#185)

--- crates/freeze/src/types/signatures.rs
+++ crates/freeze/src/types/signatures.rs
@@ -23,6 +23,12 @@ lazy_static::lazy_static! {
             .expect("Decoding failed"),
     );
 
+    /// event hash of EVENT_ERC20_APPROVAL
+    pub static ref EVENT_ERC20_APPROVAL: H256 = H256(
+        prefix_hex::decode("0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925")
+            .expect("Decoding failed"),
+    );
+
     /// event hash of EVENT_ERC721_TRANSFER
     pub static ref EVENT_ERC721_TRANSFER: H256 = H256(
         prefix_hex::decode("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef")
